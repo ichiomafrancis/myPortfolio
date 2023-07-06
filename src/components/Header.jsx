8733ed847981851ab993ce1c6f5f3c9b5aa1f828
Francis Chioma <ichiomafrancis@gmail.com>
2023-07-06 16:58:00 +0100

    installed bootstrap. Would like to use the bootstrap  offcanvas plugin in my portfolio design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,3 +9,16 @@ const Header = () => {
 };
 
 export default Header;
+{
+  /* <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
+
+<div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+  <div class="offcanvas-header">
+    <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    ...
+  </div>
+</div> */
+}
